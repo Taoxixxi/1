e55b79e354d338a1ad48eb9ec06066c63365c688
tm <1057496963@qq.com>
2018-10-09 17:24:19 +0800

    换成组件，添加路由

---
+++ src/components/header.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="header">
+    <header>
+      <div class="leftL">
+        <img class="logo-img" src="../assets/cnodejs_light.svg">
+        <el-input v-model="input" class="search-input" prefix-icon="el-icon-search"></el-input>
+      </div>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item index="2">新手入门</el-menu-item>
+        <el-menu-item index="3">API</el-menu-item>
+        <el-menu-item index="4">关于</el-menu-item>
+        <el-menu-item index="5">注册</el-menu-item>
+        <el-menu-item index="6">登录</el-menu-item>
+      </el-menu>
+    </header>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "header",
+    data() {
+      return {
+        input: '',
+        activeName: 'second',
+        activeIndex: '1',
+        activeIndex2: '1'
+      }
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      },
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .logo-img {
+    width: 115px;
+    height: 50px;
+    margin-right: 10px;
+  }
+
+  header {
+    background-color: #464646;
+  }
+
+  .search-input {
+    border-color: #c0c4cc;
+    width: 206px;
+    line-height: 45px;
+    border-radius: 26px;
+    margin: 5px 0;
+  }
+
+  .header >>> .el-input__inner {
+    border-radius: 25px;
+    line-height: 26px;
+    height: 26px;
+  }
+
+  .header >>> .el-tabs__item {
+    color: #fff;
+  }
+
+  .header >>> .el-input__icon {
+    line-height: 26px;
+  }
+
+  .el-tab-pane {
+    color: #fff;
+  }
+
+  .header >>> .el-tabs__active-bar {
+    display: none;
+  }
+
+  .header >>> .el-menu.el-menu--horizontal {
+    border: none;
+    display: inline-block;
+    background: none;
+    color: #ccc;
+  }
+
+  .header >>> .el-menu-item:hover {
+    background: none;
+    color: #fff;
+    height: 50px;
+    line-height: 50px;
+  }
+
+  .header >>> .el-menu--horizontal > .el-menu-item.is-active {
+    border: none;
+  }
+
+  .header >>> .el-menu--horizontal > .el-menu-item:not(.is-disabled):focus {
+    background: none;
+  }
+
+  .header >>> .el-menu--horizontal .el-menu-item:not(.is-disabled):focus {
+    color: cornflowerblue;
+  }
+
+  .leftL {
+    display: inline-flex;
+    vertical-align: super;
+    justify-content: center;
+    width: 50%;
+  }
+</style>
---
+++ src/components/tabs.vue
@@ -0,0 +1,70 @@
+<template>
+<div class="tabs">
+  <el-row :gutter="20">
+    <el-col :span="12" :offset="5">
+      <div class="grid-content bg-purple">
+        <ul class="second-tabs">
+          <li index="1">全部</li>
+          <li index="2">精华</li>
+          <li index="3">分享</li>
+          <li index="4">问答</li>
+          <li index="5">招聘</li>
+          <li index="6">客户端测试</li>
+        </ul>
+      </div>
+    </el-col>
+  </el-row>
+</div>
+
+</template>
+
+<script>
+    export default {
+        name: "tabs"
+    }
+</script>
+
+<style scoped>
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    width: 322.5px;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 5px;
+  }
+  .tabs >>> .el-row {
+    margin: 0 0 20px 0!important;
+  }
+
+  .el-row :last-child {
+    margin-bottom: 0;
+  }
+
+  .el-col {
+    border-radius: 4px;
+  }
+
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
+  .row-bg {
+    padding: 10px 0;
+  }
+  .second-tabs li{
+    color: #80bd01;
+  }
+</style>
